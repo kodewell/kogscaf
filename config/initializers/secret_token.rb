65f34cfcff58dcecc1bdd701e15d796d71e91eb1
kogcyc <matthewgrimm@yahoo.com>
2015-04-27 17:21:37 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scaf::Application.config.secret_token = '5690f32d4878397b5ea2357ba14ded2c2d6b67002d66e6d0569537453aaf59b2927a257af69cc95b93b73708ff5a34bc1c9310aabf43bf31e4321cf6986c6862'
